bcc0567c0ed8e26bfbb0c27b07e78d6bdfd6b6b8
Ji Shan <uug54332017@gmail.com>
2021-01-14 07:39:49 +1100

    add table and basic date manipulation

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -15,7 +15,7 @@ const CryptoCurrencies = (props) => {
       const response = await fetch("http://localhost:5000/crypto_currencies");
       const data = await response.json();
       await setCryptoCurrencies(data);
-      setShowInfo(display);
+      await setShowInfo(display);
       await setState(data);
     } catch (e) {
       console.error(e);
@@ -24,33 +24,78 @@ const CryptoCurrencies = (props) => {
   const renderCryptoCurrencies = () => {
     return cryptoCurrencies.map((data, index) => {
       return (
-        <div className="div ml-5" key={index}>
-          <p>{data.Currency}</p>
-          <p>{(Number(data.Open) - Number(data.Close)).toFixed(2)}</p>
-          
-          <p>{formattedDate(data.Date)}</p>
-        </div>
+        <tbody key={data._id}>
+          <tr>
+            <td>{index + 1}</td>
+            <td>{data.Currency}</td>
+            <td>{(Number(data.Open) - Number(data.Close)).toFixed(2)}</td>
+
+            <td>{formattedDate(data.Date)}</td>
+          </tr>
+        </tbody>
       );
     });
   };
-  const formattedDate = (date) => {
-   return moment(date).format("YYYY-MM-DD");
-  }
-console.log(formattedDate(selectedDate))
+  const formattedDate = (date, addDays) => {
+    let response = moment(date).format("YYYY-MM-DD");
+    if (addDays) {
+      const afterDate = moment(date, "DD-MM-YYY").add(addDays, "day")._d;
+      response = moment(afterDate).format("YYYY-MM-DD");
+    }
+    return response;
+  };
+
+  const oneDayDifference = () => {
+    return cryptoCurrencies.map((data, index) => {
+      if (formattedDate(data.Date) === formattedDate(selectedDate)) {
+        return (
+          <tbody key={data._id}>
+            <tr>
+              <td>{index + 1}</td>
+              <td>{data.Currency}</td>
+              <td>{"$ " + data.Close}</td>
+              <td>{data.Currency}</td>
+
+              <td>{(Number(data.Open) - Number(data.Close)).toFixed(2)}</td>
+
+              <td>{formattedDate(data.Date)}</td>
+            </tr>
+          </tbody>
+        );
+      } else return <tbody key={data._id}></tbody>;
+    });
+  };
+
   return (
     <div>
+      <DatePicker
+        selected={selectedDate}
+        onChange={(date) => setSelectedDate(date)}
+      />
       <button
         onClick={() => getCryptoCurrencies(!showInfo)}
         className="btn btn-info m-5"
       >
         Show Crypto Currencies Data
       </button>
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-      />
-      {renderCryptoCurrencies()}
-      {showInfo && renderCryptoCurrencies()}
+
+      {showInfo && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Coin</th>
+              <th>Price</th>
+              <th>24h</th>
+              <th>7d</th>
+              <th>24h Volume</th>
+              <th>Mkt Cap</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          {oneDayDifference()}
+        </table>
+      )}
     </div>
   );
 };
